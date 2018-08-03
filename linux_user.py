a78163ab3bed97c92e064ad8b19048ff8a1a1d10
zdhovey <31992960+zdhovey@users.noreply.github.com>
2018-08-03 11:14:32 -0500

    Add files via upload

---
+++ linux_user.py
@@ -0,0 +1,24 @@
+'''
+Created on Aug 3, 2018
+
+@author: Zane
+'''
+
+import sys, time
+
+print "Booting..."
+print "Enter CTRL+C to exit \n\n"
+
+def oddfunc(b):
+    print "odd function initiated. ",b+1
+def evenfunc(b):
+    print "even function initiated. ",b+1.5
+b = 0
+while True:
+    oddfunc(b)
+    time.sleep(.5)
+    evenfunc(b)
+    time.sleep(.5)
+    b += 1
+    if(b >= 3):
+        break
